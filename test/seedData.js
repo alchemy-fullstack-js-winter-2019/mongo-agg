ecf9cfd6464c25d82eb42477a2b9b4cd1f9c4443
Marcy Silverman <marcysilve225@gmail.com>
2019-01-29 14:41:26 -0800

    got 5 users

--- test/seedData.js
+++ test/seedData.js
@@ -1,16 +1,24 @@
 const Chance = require('chance');
 const chance = new Chance();
 const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
 const seedData = () => {
+    const users = [...Array(5)];
+
     const arr = [...Array(100)];
     return Promise.all(
+        users.map((n, i) => {
+            return User.create({
+                email: `${i}happy@gmail.com`, password: 'password1'
+            });
+        }),
         arr.map(() => {
             return Tweet.create({ 
                 handle: chance.name(),
                 text: chance.sentence()
             });
-        })
+        }),
     );
 };
 module.exports = seedData; 
