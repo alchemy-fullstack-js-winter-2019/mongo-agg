4c1a764fbc90de110d98e8753fa166651be1e720
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-29 14:36:07 -0800

    users and tweets are created

---
+++ server.js
@@ -0,0 +1,6 @@
+// const app = require('./lib/app');
+
+// app.listen(7890, () => {
+//     /* eslint-disable-next-line */
+//     console.log('listening on port 7890');
+// });
---
+++ test/seedData.js
@@ -0,0 +1,29 @@
+var Chance = require('chance');
+const Tweet = require('../lib/models/Tweet');
+var chance = new Chance();
+const User = require('../lib/models/User');
+
+// Instantiate Chance so it can be used
+const seedData = () => {
+    const arr = [...Array(100)];
+
+    // const users = [{ email: 'test@test.com', password: 'password' }];
+    const userArr = [...Array(5)];
+
+    
+
+    return Promise.all(
+        userArr.map(() => {
+            return User.create({
+                email: 'test@test.com', password: 'password' });
+        }),
+            
+        arr.map(() => {
+            return Tweet.create({ 
+                handle: chance.name(),
+                text: chance.sentence()
+            });
+        }));
+};
+
+module.exports = seedData;
