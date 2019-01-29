c3def04067356b6c1cde576e32dee1145493853f
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-29 15:19:05 -0800

    handle is now userId

--- test/seedData.js
+++ test/seedData.js
@@ -1,23 +1,30 @@
 const Chance = require('chance');
+const chance = new Chance();
 const Tweet = require('../lib/models/Tweet');
 const User = require('../lib/models/User');
-const chance = new Chance();
+const mongoose = require('mongoose');
+
+const users = [...Array(5)];
+const arr = [...Array(100)];
+
+mongoose.connection.dropDatabase();
 
 const createTweets = () => {
-  const usersArr = [...Array(5)];
-  const arr = [...Array(100)];
   return Promise.all(
-    usersArr.map(() => {
+    users.map((el, index) => {
       return User.create(
-        [{ email: 'test@test.com', password: 'password' }]);
-    }),
-    arr.map(() => {
-      return Tweet.create({
-        handle: chance.name(),
-        text: chance.sentence()
-      });
+        { email: `test${index}@test.com`, password: 'password' });
     })
-  );
+  )
+    .then(users => {
+      return Promise.all(
+        arr.map(() => {
+          Tweet.create({ 
+            handle: chance.pickone(users)._id, 
+            text: chance.sentence() });
+        })
+      );
+    });
 };
 
 module.exports = createTweets;
