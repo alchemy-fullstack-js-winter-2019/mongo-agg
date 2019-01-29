2f8740155e7891d018cff6cb476853fa463b0856
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-29 15:40:19 -0800

    creating user model

--- test/seedData.js
+++ test/seedData.js
@@ -1,14 +1,25 @@
 const Chance = require('chance');
 const chance = new Chance();
 const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
 
 
 
 module.exports = () => {
+
   return Promise.all(
-    [...Array(100)].map(() => Tweet.create({ handle: chance.pickone(['abel', 'another', 'a third']),
-     text: chance.sentence() 
-    })
-    ));
+    [...Array(5)].map(() => User.create({ email: 'test@test.com' }))
+  )
+    .then(users => {
+      return Promise.all(
+        [...Array(100)].map(() => Tweet.create({ handle: chance.pickone([users._id]),
+          text: chance.sentence() 
+        })
+        ));
+
+    });
+
+
+
 };
