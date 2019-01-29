d7080f0adb2bd0adb470eacd91ae82bab0f21e3a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-29 14:11:03 -0800

    I HAVE MULTIPLE USERS

--- test/seedData.js
+++ test/seedData.js
@@ -1,22 +1,28 @@
 const Chance = require('chance');
 const chance = new Chance();
 const Tweet = require('../lib/models/Tweet');
-// const User = require('../lib/models/User');
+const User = require('../lib/models/User');
 
-// const users = [{ email: 'test@test.com', password: 'password' }];
+const users = [{ email: 'test1@test.com', password: 'password' }, { email: 'test2@test.com', password: 'password' }, { email: 'test3@test.com', password: 'password' }, { email: 'test4@test.com', password: 'password' }, { email: 'test5@test.com', password: 'password' }];
 const arr = [...Array(100)];
+
 const seedData = () => {
-  
   return Promise.all(
-    arr.map(() => {
-      return Tweet.create({ 
-        handle: chance.name(), 
-        text: chance.sentence() 
-      });
+    users.map(user => {
+      return User.create(user)
+        .then(() => {
+          return Promise.all(
+            arr.map(() => {
+              return Tweet.create({ 
+                handle: chance.name(), 
+                text: chance.sentence() 
+              });
+            })
+          );
+        });
+      
     })
   );
 };
 
 module.exports = seedData;
-
-console.log(seedData());
