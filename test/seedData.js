c7ac5fd6842b66c19d7036ceaafaa98c49079ac6
pickuppatterns <pickup.patterns@gmail.com>
2019-01-29 16:45:09 -0800

    updates tweets aggreation, robo3t reports total #

---
+++ test/seedData.js
@@ -0,0 +1,23 @@
+const Tweet = require('../lib/model/Tweet');
+const User = require('../lib/model/User');
+const Chance = require('chance');
+const chance = new Chance();
+
+const TOTAL_USERS = 10;
+const TOTAL_TWEETS = 100;
+
+module.exports = () => {
+  return Promise.all(
+    [...Array(TOTAL_USERS)].map((ele, i) => User.create({ email: `test${i}@test.com` }))
+  )
+    .then(users => {
+      return Promise.all(
+        [...Array(TOTAL_TWEETS)].map(() => {
+          return Tweet.create({
+            handle: chance.pickone(users)._id,
+            text: chance.sentence()
+          });
+        })
+      );
+      });
+    };
