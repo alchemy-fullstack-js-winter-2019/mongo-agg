3b4ef31e95d838e914733da8cd04d1ddad1d18f9
Kate Dameron <katerj33@gmail.com>
2019-01-29 16:59:50 -0800

    seed data created

--- test/seedData.js
+++ test/seedData.js
@@ -1,12 +1,22 @@
 const Chance = require('chance');
 const chance = new Chance();
 const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
 module.exports = () => {
   return Promise.all(
-    [...Array(100)].map(() => {
-      const handle = chance.name();
+    [...Array(5)].map(() => {
+      const email = chance.email();
+      const password = chance.hash({ length: 15 });
+
+      return User.create({ email, password });
+    })
+  )
+  .then(users => {
+    return Promise.all([...Array(100)].map(() => {
       const text = chance.sentence({ words: 5 });
-        return Tweet.create({ handle, text });
+
+      return Tweet.create({ handle: chance.pickone(users)._id, text });
     }));
+  })
 };
