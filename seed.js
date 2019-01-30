e3bdab7bec6e3ce3dfeedf639db03004d535a55d
Marcy Silverman <marcysilve225@gmail.com>
2019-01-29 16:55:12 -0800

    fixed eslint

--- seed.js
+++ seed.js
@@ -4,6 +4,7 @@ const seedData = require('./test/seedData');
 const mongoose = require('mongoose');
 
 seedData()
+    /* eslint-disable-next-line */
     .then(() => console.log('done'))
     .finally(() => mongoose.connection.close());
 
