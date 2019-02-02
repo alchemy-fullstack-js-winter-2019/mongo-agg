34d36931a4f66ac05e1596ede03c473fa87d49f6
Carmen Perezchica <crperezchica@gmail.com>
2019-02-01 22:06:07 -0800

    updated package.json to pass travis

--- test/seedData.js
+++ test/seedData.js
@@ -5,7 +5,7 @@ const chance = new Chance();
 
 module.exports = () => {
   return Promise.all(
-    [...Array(10)].map((ele, i) => User.create({ email: `test${i}@test.com`, password: 'password' }))
+    [...Array(10)].map((ele, i) => User.create({ email: `test${i}@test.com`, password: 'password' })) //map allows you to read the index of the item/element
   )
     .then(users => {
       return Promise.all([...Array(100)].map(() => Tweet.create({ handle: chance.pickone(users)._id, text: chance.sentence() }))); //for each item in the array create a new tweet   
