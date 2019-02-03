85575a9d9152c4e2709ecc6a8d08d140332ade33
JeiWolfei <jvampy22@gmail.com>
2019-02-03 13:23:37 -0800

    fixed seed data

--- test/seedData.js
+++ test/seedData.js
@@ -1,27 +1,28 @@
-const Tweet = require('../lib/models/Tweet');
-const User = require('../lib/models/User');
 const Chance = require('chance');
 const chance = new Chance();
+const Tweet = require('../lib/models/Tweet');
+const User = require('../lib/models/User');
 
 const users = [...Array(5)];
 const arr = [...Array(100)];
 
+
 const createTweets = () => {
   return Promise.all(
     users.map((el, index) => {
-      return User.create({ email: `seed${i}@test.com`,
-        password: 'password' });
+      return User.create(
+        { email: `test${index}@test.com`, password: 'password' });
     })
   )
     .then(users => {
       return Promise.all(
         arr.map(() => {
-          Tweet.create({
-            handle: chance.pickone(users)._id,
+          Tweet.create({ 
+            handle: chance.pickone(users)._id, 
             text: chance.sentence() });
         })
       );
     });
 };
-    
+
 module.exports = createTweets;
