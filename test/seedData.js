eb2a717c3b37f353b3b3c355e148396f8de29e37
JeiWolfei <jvampy22@gmail.com>
2019-02-03 19:49:25 -0800

    refactored seed data

--- test/seedData.js
+++ test/seedData.js
@@ -8,16 +8,15 @@ const TOTAL_TWEETS = 100;
 
 module.exports = () => {
   return Promise.all(
-    [...Array(Total_USERS)].map((el, i) => {
-      User.create({ email: `test${i}@test.com` }))
-    )
+    [...Array(TOTAL_USERS)].map((el, i) =>  User.create({ email: `test${i}@test.com` }))
+  )
     .then(users => {
       return Promise.all(
         [...Array(TOTAL_TWEETS)].map(() => {
           return Tweet.create({ 
             handle: chance.pickone(users)._id, 
             text: chance.sentence()
-           });
+          });
         })
       );
     });
